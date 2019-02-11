dfafc621caaa422944bf87274d390b62acf81116
kojiyamauchi <all.the.small.things@me.com>
2019-02-11 14:15:12 +0900

    added nuxt starter kit.

---
+++ apps/store/index.ts
@@ -0,0 +1,42 @@
+import { MutationTree, GetterTree, ActionTree } from 'vuex'
+
+interface State {
+  // Add Interfaces on State.
+  isLandscape: boolean
+  isResize: boolean
+  stateDataBase: {}
+}
+
+export const state = (): State => ({
+  // Add State.
+  isLandscape: false,
+  isResize: false,
+  stateDataBase: {}
+})
+
+export const mutations: MutationTree<State> = {
+  // Add Mutations.
+  onLandscape(stateArg: State) {
+    stateArg.isLandscape = true
+  },
+  offLandscape(stateArg: State) {
+    stateArg.isLandscape = false
+  },
+  onResize(stateArg: State) {
+    stateArg.isResize = true
+  },
+  offResize(stateArg: State) {
+    stateArg.isResize = false
+  },
+  onDataBase(stateArg: State, payload: any) {
+    stateArg.stateDataBase = payload
+  }
+}
+
+export const getters: GetterTree<State, State> = {
+  // Add Getters.
+}
+
+export const actions: ActionTree<State, State> = {
+  // Add Actions.
+}
