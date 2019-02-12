2f22b82c5575e5dc5263e6622fc5a88f105c6bf2
kojiyamauchi <all.the.small.things@me.com>
2019-02-12 15:33:09 +0900

    added pages/PlayGroundOfGet.vue

---
+++ apps/pages/PlayGroundOfGet.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+  .playground-of-get
+    span.todo
+      | TODO.
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+@Component({})
+export default class PlayGroundOfGet extends Vue {
+  // Types.
+
+  constructor() {
+    super()
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.playground-of-get
+  width 100%
+  height 100vh
+  display flex
+  justify-content center
+  align-items center
+.todo
+  transform translateY(-60%)
+</style>
