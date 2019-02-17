83067b08fea32fa8bae6c18a87651353851905bc
kojiyamauchi <all.the.small.things@me.com>
2019-02-17 21:05:16 +0900

    update components/OAuth/GetToken.vue

--- apps/components/OAuth/GetToken.vue
+++ apps/components/OAuth/GetToken.vue
@@ -28,9 +28,12 @@ export default class GetToken extends Vue {
   border solid 1px rgba(0,0,0,0.5)
   border-radius 4px
   box-shadow 1px 1px 1px rgba(0,0,0,0.25)
+  transition border 0.25s ease-in
   +isDesktop()
     width auto
     min-width 462px
   &:hover
     cursor pointer
+    border solid 1px rgba(0, 241, 255, 0.5)
+    transition border 0.25s ease-out
 </style>
